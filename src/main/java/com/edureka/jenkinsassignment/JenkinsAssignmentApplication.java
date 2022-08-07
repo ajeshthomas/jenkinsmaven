570234fa9cdb545b248359613ce90394645eec45
ajeshthomas <thomas.ajesh@gmail.com>
2022-08-07 09:34:46 +0000

    initial commit

---
+++ src/main/java/com/edureka/jenkinsassignment/JenkinsAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.edureka.jenkinsassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAssignmentApplication.class, args);
+		System.out.println("############## INSIDE JENKINS ASSIGNMENT #############");
+	}
+
+}
